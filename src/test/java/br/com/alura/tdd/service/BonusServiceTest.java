f2018b52cdf61166bd111c7027472d7765826637
felipecabesssa <felipesoares100@hotmail.com>
2022-01-11 20:04:21 -0300

    TDD - Test-Driven Development

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -14,24 +14,24 @@ class BonusServiceTest {
 	@Test
 	void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Felipe", LocalDate.now(), new BigDecimal("25000")));
-		
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+
 		assertEquals(new BigDecimal("0.00"), bonus);
 	}
-	
+
 	@Test
 	void bonusDeveriaSer10PorCentoDoSalario() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Felipe", LocalDate.now(), new BigDecimal("2500")));
-		
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500")));
+
 		assertEquals(new BigDecimal("250.00"), bonus);
 	}
-	
+
 	@Test
-	void bonusDeveriaSerDezPorCentoParaSalarioDeExatamete10000() {
+	void bonusDeveriaSerDezPorCentoParaSalarioDeExatamente10000() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Felipe", LocalDate.now(), new BigDecimal("10000")));
-		
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10000")));
+
 		assertEquals(new BigDecimal("1000.00"), bonus);
 	}
 
